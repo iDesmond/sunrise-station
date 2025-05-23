4f145d779d19eebe611215d2297fc0851adfd765
iDesmond <kdakdo2014@gmail.com>
2025-05-24 02:22:24 +0300

    Консоль радио и доступы

--- Content.Server/Lathe/LatheSystem.cs
+++ Content.Server/Lathe/LatheSystem.cs
@@ -380,14 +380,14 @@ namespace Content.Server.Lathe
                 if (!_proto.TryIndex(recipeId, out LatheRecipePrototype? recipe))
                     continue;
 
-                var itemName = GetRecipeName(recipe!);
-                recipeNames.Add(Loc.GetString("lathe-unlock-recipe-radio-broadcast-item", ("item", itemName)));
+                //var itemName = GetRecipeName(recipe!);
+                //recipeNames.Add(Loc.GetString("lathe-unlock-recipe-radio-broadcast-item", ("item", itemName)));
             }
 
             if (recipeNames.Count == 0)
                 return;
 
-            var message =
+            /*var message =
                 recipeNames.Count > ent.Comp.MaximumItems ?
                     Loc.GetString(
                         "lathe-unlock-recipe-radio-broadcast-overflow",
@@ -402,7 +402,7 @@ namespace Content.Server.Lathe
             foreach (var channel in ent.Comp.Channels)
             {
                 _radio.SendRadioMessage(ent.Owner, message, channel, ent.Owner, escapeMarkup: false);
-            }
+            }*/
         }
 
         private void OnResearchRegistrationChanged(EntityUid uid, LatheComponent component, ref ResearchRegistrationChangedEvent args)
